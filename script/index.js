94f5a70e1b6e2aff65e00637f15ba54bf3edde6d
Suraj KY <face.surajyadav@gmail.com>
2021-01-13 23:38:15 +0530

    modified index.html & modified index.js(add function of mouseover/mouseout).

--- script/index.js
+++ script/index.js
@@ -1,4 +1,22 @@
 var topLeft = 0, topCenter = 0, topRight = 0, centerLeft = 0, centerCenter = 0,
 centerRight = 0, bottomLeft = 0, bottomCenter = 0, bottomRight = 0;
 
-alert("hello");
+var chance = 0;
+
+var topLeftImgHoverCircle = document.getElementById('top-left-img-circle-hover');
+var topLeftImgHoverCross = document.getElementById('top-left-img-cross-hover');
+
+function hoverTopLeftOver() {
+  if(topLeft == 0) {
+    if(chance == 0) {
+      topLeftImgHoverCross.style.display = "block";
+    } else {
+      topLeftImgHoverCircle.style.display = "block";
+    }
+  }
+}
+
+function hoverTopLeftOut() {
+  topLeftImgHoverCross.style.display = "none";
+  topLeftImgHoverCircle.style.display  = "none";
+}
