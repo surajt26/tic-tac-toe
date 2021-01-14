032cb8665dc55b9083581e80124c803e2e066536
Suraj KY <face.surajyadav@gmail.com>
2021-01-14 12:35:59 +0530

    complete the function for topLeft block

--- script/index.js
+++ script/index.js
@@ -1,11 +1,20 @@
+/* fill values for all nine blocks */
 var topLeft = 0, topCenter = 0, topRight = 0, centerLeft = 0, centerCenter = 0,
 centerRight = 0, bottomLeft = 0, bottomCenter = 0, bottomRight = 0;
 
+/* chance value */
 var chance = 0;
 
-var topLeftImgHoverCircle = document.getElementById('top-left-img-circle-hover');
-var topLeftImgHoverCross = document.getElementById('top-left-img-cross-hover');
+/* chance count */
+var count = 0;
 
+/* for top left block */
+var topLeftImgCross = document.getElementById('top-left-img-cross');
+var topLeftImgHoverCross = document.getElementById('top-left-img-cross-hover');
+var topLeftImgCircle = document.getElementById('top-left-img-circle');
+var topLeftImgHoverCircle = document.getElementById('top-left-img-circle-hover');
+
+/* function for block over */
 function hoverTopLeftOver() {
   if(topLeft == 0) {
     if(chance == 0) {
@@ -13,10 +22,33 @@ function hoverTopLeftOver() {
     } else {
       topLeftImgHoverCircle.style.display = "block";
     }
+    console.log("hover work");
   }
 }
 
+/* function for block out */
 function hoverTopLeftOut() {
-  topLeftImgHoverCross.style.display = "none";
-  topLeftImgHoverCircle.style.display  = "none";
+  if(topLeft == 0) {
+    topLeftImgHoverCross.style.display = "none";
+    topLeftImgHoverCircle.style.display  = "none";
+    console.log("hover out work");
+  }
+}
+
+/* fucntion for block fill */
+function fillTopLeft() {
+  if(topLeft == 0) {
+    if(chance == 0) {
+      topLeftImgCross.style.display = "block";
+      topLeftImgHoverCross.style.display = "none";
+      topLeft = 1;
+    } else {
+      topLeftImgCircle.style.display = "block";
+      topLeftImgHoverCircle.style.display  = "none";
+      topLeft = 2;
+    }
+    chance = 1;
+    count++;
+    console.log("fill work");
+  }
 }
